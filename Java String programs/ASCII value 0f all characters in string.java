d3ee8f6ac3de9acf0ef9067d4e05c9673b1ab81f
sreejareddy993 <sreejareddy993@gmail.com>
2025-06-22 21:17:19 +0530

    Create ASCII value 0f all characters in string.java

---
+++ Java String programs/ASCII value 0f all characters in string.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+for(int i=0;i<s.length();i++){
+char ch=s.charAt(i);
+int ans=ch;
+System.out.print(ans+" ");
+}
+}
+}
+ 
